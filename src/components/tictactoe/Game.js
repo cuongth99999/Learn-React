18376a8b6fb03886057375f3aaa3adaa26b53ba3
cuongth <cuongth@magenest.com>
2023-07-31 10:43:00 +0700

    update game Tictactoe use hook useReducer

--- src/components/tictactoe/Game.js
+++ src/components/tictactoe/Game.js
@@ -1,37 +1,109 @@
-import React, { useState } from 'react';
+import React, { useReducer, useState } from 'react';
 import Board from './Board';
 import "./GameStyle.css";
 import { calculateWinner } from '../../helper';
 
+// innitialState = {}
+
+const initialState = {
+    board: Array(9).fill(null),
+    xIsNext: true
+};
+
+// immutable
+
+// deep copy JSON.parse(JSON.stringify(obj))
+
+const gameReducer = (state, action) => {
+    switch (action.type) {
+        case 'CLICK' : {
+            const {board, xIsNext} = state;
+            const {index, winner} = action.payload;
+            if (winner || board[index]) return state;
+            const nextState = JSON.parse(JSON.stringify(state));
+            nextState.board[index] = xIsNext ? 'X' : 'O';
+            nextState.xIsNext = !xIsNext;
+
+            return nextState;
+        }
+
+        case 'RESET' : {
+            const nextState = JSON.parse(JSON.stringify(state));
+            nextState.board = Array(9).fill(null);
+            nextState.xIsNext = true;
+            return nextState;
+        }
+        default :
+            break;
+    }
+
+    return state;
+}
+
 const Game = () => {
-    const [board, setBoard] = useState(Array(9).fill(null));
+    // const [board, setBoard] = useState(Array(9).fill(null));
 
-    const [xIsNext, setxIsNext] = useState(true);
+    // const [xIsNext, setxIsNext] = useState(true);
 
-    const winner = calculateWinner(board);
+    // useReducer
+
+    const [state, dispatch] = useReducer(gameReducer, initialState);
+
+    // const action = {type: 'CLICK', payload:{}};
+
+    // const [state, setState] = useState({
+    //     board: Array(9).fill(null),
+    //     xIsNext: true
+    // });
+
+    const winner = calculateWinner(state.board);
 
     const handleClick = (index) => {
-        const boardCoppy = [...board];
-        if (winner || boardCoppy[index]) {
-            return;
-        }
-        boardCoppy[index] = xIsNext ? 'X' : 'O';
-        setBoard(boardCoppy);
-        setxIsNext(!xIsNext);
+        // const boardCoppy = [...state.board];
+        // if (winner || boardCoppy[index]) {
+        //     return;
+        // }
+
+        // boardCoppy[index] = state.xIsNext ? 'X' : 'O';
+        // setBoard(boardCoppy);
+        // setxIsNext(!xIsNext);
+
+        // setState({
+        //     ...state,
+        //     board: boardCoppy,
+        //     xIsNext: !state.xIsNext,
+        // });
+
+        dispatch({
+            type: 'CLICK',
+            payload: {
+                index,
+                winner
+            }
+        })
     };
 
     const handleRestGame = () => {
-        setBoard(Array(9).fill(null));
-        setxIsNext(true);
+        // setBoard(Array(9).fill(null));
+        // setxIsNext(true);
+        // setState({
+        //     ...state,
+        //     board: Array(9).fill(null),
+        //     xIsNext: true,
+        // });
+
+        dispatch({
+            type: 'RESET'
+        })
     }
 
     return (
         <div className="game">
             <Board 
-                cells={board} 
+                cells={state.board} 
                 onClick={handleClick}></Board>
             <a href="#" className="button" onClick={handleRestGame}>Reset game</a><br/>
-            {winner ? `Winner is ${winner ? 'X' : 'O'}` : ''}
+            {`Winner is ${winner ? winner : ''}`}
         </div>
     );
 };
